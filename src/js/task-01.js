ca10d1685849e1d2d2271645b75031c21941c282
Igor-Konyaga <konyaga.igor10@gmail.com>
2023-07-16 19:11:05 +0300

    Task 1

---
+++ src/js/task-01.js
@@ -0,0 +1,14 @@
+/*----------------------- Завдання 1 ---------------------*/
+
+const itemsEl = document.querySelectorAll('.item');
+console.log(`Number of categories: ${itemsEl.length}`);
+
+itemsEl.forEach(el => {
+	console.log(`Category: ${el.firstElementChild.textContent}`);
+	console.log(`Elements: ${el.lastElementChild.children.length}`);
+});
+
+/*---------------------------------------------------------*/
+
+/*----------------------- Завдання 2 ---------------------*/
+
