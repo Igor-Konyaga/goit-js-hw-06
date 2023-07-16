d4841812f326ad92efeb5192b05f7450998d697a
Igor-Konyaga <konyaga.igor10@gmail.com>
2023-07-16 21:23:17 +0300

    Task 2 and 3

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.getElementById("ingredients");
+
+const listEl = ingredients.map(ing => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add('item');
+  itemEl.textContent = ing;
+  return itemEl;
+});
+
+ingredientsEl.append(...listEl);
+
