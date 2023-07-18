aa44f1532ad633aaef8156e6deecfa427e9e867f
Igor-Konyaga <konyaga.igor10@gmail.com>
2023-07-19 00:46:25 +0300

    Task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,30 @@
+const formEl = document.querySelector('.login-form');
+
+
+formEl.addEventListener('submit', handleSubmissionForm)
+
+function handleSubmissionForm (e) {
+	e.preventDefault();
+
+const currentEmailLength = e.currentTarget.elements.email.value.length;
+const currentPasswordLength = e.currentTarget.elements.password.value.length;
+
+
+if (currentEmailLength < 1 || currentPasswordLength < 1) {
+	alert('Усі поля повинні бути заповлені!!!')
+};
+
+const formData = {
+	email: `${e.currentTarget.elements.email.value}`,
+	password: `${e.currentTarget.elements.password.value}`,
+}
+
+console.log(formData);
+
+e.currentTarget.reset();
+};
+
+
+
+
 
